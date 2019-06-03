22e247e45d6cb39d9676d03be9e593a7b3fd3037
Peters,Wessel W.J.H <w.peters@student.fontys.nl>
2019-06-03 14:43:01 +0200

    Added working ipv6 support.

--- build_all_ipv6.sh
+++ build_all_ipv6.sh
@@ -9,7 +9,7 @@ wg genkey | tee server_private_key | wg pubkey > server_public_key
 PrivateKey=`cat server_private_key`
 cat <<EOF >/etc/wireguard/wg0.conf
 [Interface]
-Address = 10.200.200.1/24, fe80:8:8:8::1/64
+Address = 10.200.200.1/24, fd42:42:42::1/64
 SaveConfig = true
 PrivateKey = $PrivateKey
 ListenPort = 51820
@@ -28,22 +28,29 @@ echo $networkName
 
 #set up the IP tables for wireguard
 iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+ip6tables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+ip6tables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 iptables -A INPUT -p udp -m udp --dport 51820 -m conntrack --ctstate NEW -j ACCEPT
 iptables -A INPUT -s 10.200.200.0/24 -p tcp -m tcp --dport 53 -m conntrack --ctstate NEW -j ACCEPT
+ip6tables -A INPUT -s fd42:42:42::0/64 -p tcp -m tcp --dport 53 -m conntrack --ctstate NEW -j ACCEPT
 iptables -A INPUT -s 10.200.200.0/24 -p udp -m udp --dport 53 -m conntrack --ctstate NEW -j ACCEPT
+ip6tables -A INPUT -s fd42:42:42::0/64 -p udp -m udp --dport 53 -m conntrack --ctstate NEW -j ACCEPT
 iptables -A FORWARD -i wg0 -o $networkName -m conntrack --ctstate NEW -j ACCEPT
+ip6tables -A FORWARD -i wg0 -o $networkName -m conntrack --ctstate NEW -j ACCEPT
 
 #get the current used network to make sure the vpn traffic gets routed to the correct network
 
 iptables -t nat -A POSTROUTING -s 10.200.200.0/24 -o $networkName -j MASQUERADE
-ip6tables -t nat -A POSTROUTING -s fe80:8:8:8::0/64 -o $networkName -j MASQUERADE
+ip6tables -t nat -A POSTROUTING -s fd42:42:42::0/64 -o $networkName -j MASQUERADE
 
 
 #install the web environment prerequisites 
 apt-get install -y apache2 -qq > /dev/null
 apt-get install -y qrencode -qq > /dev/null
 apt-get install -y php -qq > /dev/null
+apt-get install -y php-mysql -qq > /dev/null
+apt-get install -y mysql-server -qq > /dev/null
 
 #set up the correct firewall routes TODO: improve and check which ports need to be open
 echo "y" | ufw enable
@@ -62,7 +69,7 @@ ufw reload
 
 #set the route again otherwise this rule is overwriten TODO: check if there is a better way
 iptables -t nat -A POSTROUTING -s 10.200.200.0/24 -o $networkName -j MASQUERADE
-ip6tables -t nat -A POSTROUTING -s fe80:8:8:8::0/64 -o $networkName -j MASQUERADE
+ip6tables -t nat -A POSTROUTING -s fd42:42:42::0/64 -o $networkName -j MASQUERADE
 
 #add certain commands for the www-data to manage the WireGuard interface
 echo 'www-data  ALL=(ALL) NOPASSWD: /usr/bin/wg' >> /etc/sudoers
@@ -76,7 +83,7 @@ chown -R www-data:www-data /var/www/html
 ipadresses=`hostname -i`
 IFS=' ' read -r -a array <<< "$ipadresses"
 ipv6=`echo "${array[0]}"`
-ipv4=`echo "${array[1]}"`
+ipv4=`echo "${array[2]}"`
 
 cat <<EOF >/var/www/html/index.php
 <html><head><title>eduVPN QR code PoC</title></head>
@@ -114,20 +121,14 @@ if (!file_exists(\$currentIP)) {
 	
 	//Create the content for the .conf file that WireGuard uses
 \$QR_code_content = '[Interface]
-Address = 10.200.200.'.\$ip_count.'/32, fe80:8:8:8::'.\$ip_count.'/128
+Address = 10.200.200.'.\$ip_count.'/32, fd42:42:42::'.\$ip_count.'/128
 PrivateKey = '.\$privateKey.'DNS = 1.1.1.1
 ListenPort = 51820
 
 [Peer]
 PublicKey = $publicKey
-Endpoint = $ipv4:51820
-AllowedIPs = 0.0.0.0/0
-PersistentKeepalive = 25
-
-[Peer]
-PublicKey = $publicKey
-Endpoint = [$ipv6]:51820
-AllowedIPs = ::/0
+Endpoint = $ipv6:51820
+AllowedIPs = 0.0.0.0/0, ::/0
 PersistentKeepalive = 25';
 	
 	//Create a .conf file for WireGuard so that it can correctly parse into a QR code
@@ -136,7 +137,7 @@ PersistentKeepalive = 25';
 	fwrite(\$handle, \$QR_code_content);
 	fclose(\$handle);
 	
-	shell_exec('sudo wg set wg0 peer '.\$publicKey.' allowed-ips 10.200.200.'.\$ip_count.'/32,fe80:8:8:8::'.\$ip_count.'/128');
+	shell_exec('sudo wg set wg0 peer '.\$publicKey.' allowed-ips 10.200.200.'.\$ip_count.'/32,fd42:42:42::'.\$ip_count.'/128');
 
 	//Create a QR code that people can use to connect to the server.
 	shell_exec('qrencode -s 10 -d 300 -t png < '.\$currentIP.'/tmp.conf -o '.\$currentIP.'/tmp.png');
