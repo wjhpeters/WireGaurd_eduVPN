e88bfe56168c1a8e0209791927769ed262860694
Peters,Wessel W.J.H <w.peters@student.fontys.nl>
2019-06-27 15:02:10 +0200

    Small fix

--- build_all_ipv6.sh
+++ build_all_ipv6.sh
@@ -64,8 +64,8 @@ IFS=' ' read -r -a array <<< "$ipadresses"
 ipv6=`echo "${array[0]}"`
 ipv4=`echo "${array[2]}"`
 
-mysql -e "CREATE USER ${username}@localhost IDENTIFIED BY '${password}';"
-mysql -e "grant all privileges on *.* to ${username}@localhost;"
+mysql -uroot -p "CREATE USER ${username}@localhost IDENTIFIED BY '${password}';"
+mysql -uroot -p "grant all privileges on *.* to ${username}@localhost;"
 
 mysql -u$username -p$password <<MY_QUERY
 CREATE DATABASE WireGuardDB;
